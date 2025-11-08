247ec7b27b5bf4e0e246c0ad93080a9b05c41bfb
Paul Olteanu <170131110+cmpwi@users.noreply.github.com>
2025-11-07 22:03:19 -0500

    The great pledge(2) semi-rewrite
    * Separate data grabbing from data printing, thus allowing pledge(2) to be properly integrated.
    * Be paranoid and check for integer overflows when working with host data and memory data from sysctl(2).
    * Size arguments passed to calloc(3) that haven't been run through ckd_* assume that the magic constants in sys/*.h will never change types to a larger value (they haven't for 40 years, so it's a fair assumption.)
    * As a result of using ckd_*, require Clang 23 to build in the Makefile.
    * No more fancy pseudo-dynamic uptime printing, as that would make string parsing logic more complicated than it already is.
    * Minor refactoring.

--- fetch.c
+++ fetch.c
@@ -21,88 +21,126 @@
 #include <sys/sysctl.h>
 #include <sys/utsname.h>
 #include <time.h>
-
-#define NEWLINE putchar('\n')
+#include <stdckdint.h>
+#include <math.h>
 
 void
-printUserData(void)
+getUserData(char **userDataString)
 {
 	struct passwd *pw = nullptr;
 	char hostname[HOST_NAME_MAX + 1] = { 0 };
+	short userDataStringLength = 0;
+	char *u = nullptr;
 
 	if ((pw = getpwuid(getuid())) == nullptr)
-		errx(1, "getpwnam");
+		err(1, "getpwuid");
 
 	if (gethostname(hostname, HOST_NAME_MAX + 1) == -1)
-		err(1, nullptr);
+		err(1, "gethostname");
 
-	fputs(pw->pw_name, stdout); putchar('@');
-	fputs(hostname, stdout);
+	userDataStringLength = strnlen(pw->pw_name, _PW_NAME_LEN + 1) +
+				strnlen(hostname, HOST_NAME_MAX + 1) +
+				2; /* '@', and '\0' */
+
+	if ((u = calloc(1, userDataStringLength)) == nullptr)
+		err(1, "calloc");
+
+	snprintf(u, userDataStringLength, "%s@%s", pw->pw_name, hostname);
+	*userDataString = u;
 }
 
 void
-printSystemData(void)
+getSystemData(char **systemDataString)
 {
 	struct utsname u;
+	int systemDataStringLength = 0;
+	const char systemTag[] = "System:\t";
+	char *s = nullptr;
 
 	if (uname(&u) == -1)
-		err(1, nullptr);
+		err(1, "uname");
 
-	fputs("System:\t", stdout);
-	fputs(u.sysname, stdout); putchar(' ');
-	fputs(u.release, stdout); putchar(' ');
-	fputs(u.machine, stdout);
+	systemDataStringLength = strlen(systemTag) +
+				strnlen(u.sysname, SYS_NMLN) +
+				strnlen(u.release, SYS_NMLN) +
+				strnlen(u.machine, SYS_NMLN) +
+				3;
+
+	if ((s = calloc(1, systemDataStringLength)) == nullptr)
+		err(1, "calloc");
+
+	snprintf(s, systemDataStringLength, "%s%s %s %s", systemTag, u.sysname,
+							u.release, u.machine);
+	*systemDataString = s;
 }
 
 void
-printHostData(void)
+getHostData(char **hostDataString)
 {
-	int	mib[2] = { CTL_HW, HW_VENDOR };
-	char	*hwVendor = nullptr, *hwProduct = nullptr;
-	long	hwVendorLength = 0, hwProductLength = 0;
+	int mib[2] = { 0 };
+	long hwVendorLength = 0, hwProductLength = 0;
+	long hostDataStringLength = 0;
+	long insertPoint = 0;
+	const char hostTag[] = "Host:\t";
+	char *h = nullptr;
 
+	mib[0] = CTL_HW;
+	mib[1] = HW_VENDOR;
 	if (sysctl(mib, 2, nullptr, &hwVendorLength, nullptr, 0) == -1)
 		err(1, "sysctl");
 
-	if ((hwVendor = malloc(sizeof(char) * hwVendorLength)) == nullptr)
-		err(1, nullptr);
-
-	if (sysctl(mib, 2, hwVendor, &hwVendorLength, nullptr, 0) == -1)
+	mib[1] = HW_PRODUCT;
+	if (sysctl(mib, 2, nullptr, &hwProductLength, nullptr, 0) == -1)
 		err(1, "sysctl");
 
+	/*
+	 * sysctl length includes the '\0'; providing us room for one ' '
+	 * and one '\0' by default.
+	 */
+	if (ckd_add(&hostDataStringLength,
+				hwVendorLength,
+				hwProductLength) ||
+	    ckd_add(&hostDataStringLength,
+				hostDataStringLength,
+				strlen(hostTag)))
+		errx(1, "ckd_add: integer overflow");
+
+	if ((h = calloc(1, hostDataStringLength)) == nullptr)
+		err(1, "calloc");
+
+	snprintf(h, strlen(hostTag) + 1, "%s", hostTag);
+	insertPoint += strlen(hostTag);
+
+	mib[1] = HW_VENDOR;
+	if (sysctl(mib, 2, h + insertPoint, &hwVendorLength, nullptr, 0) == -1)
+		err(1, "sysctl");
+	insertPoint += hwVendorLength;
+
+	h[insertPoint - 1] = ' ';
+
 	mib[1] = HW_PRODUCT;
-	
-	if (sysctl(mib, 2, nullptr, &hwProductLength, nullptr, 0) == -1)
-		err(1, nullptr);
+	if (sysctl(mib, 2, h + insertPoint, &hwProductLength, nullptr, 0) == -1)
+		err(1, "sysctl");
 
-	if ((hwProduct = malloc(sizeof(char) * hwProductLength)) == nullptr)
-		err(1, nullptr);
-
-	if (sysctl(mib, 2, hwProduct, &hwProductLength, nullptr, 0) == -1)
-		err(1, nullptr);
-
-	fputs("Host:\t", stdout);
-	fputs(hwVendor, stdout); putchar(' ');
-	fputs(hwProduct, stdout);
-
-	if (hwVendor != nullptr)
-		free(hwVendor);
-
-	if (hwProduct != nullptr)
-		free(hwProduct);
+	*hostDataString = h;
 }
 
+/* Approximate number of characters required to store an integer. */
+#define NUM_LENGTH(x) ((floor(log10(x))) + 1)
+
 void
-printUptimeData(void)
+getUptimeData(char **uptimeDataString)
 {
 	struct timespec	boottime;
-	time_t		uptime;
-	int		timeVals[4] = { 0 };
-        const char	*timeNames[] = {"days", "hours", "minutes", "seconds"};
-	bool didPrint = false;
+	time_t uptime;
+	time_t timeVals[4] = { 0 };
+	const char *timeNames[] = {"days", "hours", "minutes", "seconds"};
+	short uptimeDataStringLength = 0;
+	const char uptimeTag[] = "Uptime:\t";
+	char *u = nullptr;
 
 	if (clock_gettime(CLOCK_BOOTTIME, &boottime) == -1)
-		err(1, nullptr);
+		err(1, "clock_gettime");
 	else
 		uptime = boottime.tv_sec;
 
@@ -113,67 +151,108 @@ printUptimeData(void)
         uptime -= timeVals[1] * 3600;
         timeVals[2] = uptime / 60;
         uptime -= timeVals[2] * 60;
-        timeVals[3] = (int)uptime;
+        timeVals[3] = uptime;
 
-	fputs("Uptime:\t", stdout);
+	uptimeDataStringLength = strlen(uptimeTag);
+	for (short i = 0; i < 4; ++i)
+		uptimeDataStringLength += NUM_LENGTH(timeVals[i]) + 1 +
+					  strlen(timeNames[i]);
+	uptimeDataStringLength += 7; /* spaces and commas */
 
-	for (int i = 0; i < 4; ++i)
-		if (timeVals[i] != 0) {
-			printf(didPrint ? ", %d %.*s" : "%d %.*s",
-				timeVals[i],
-					timeVals[i] == 1 ?
-					(int)strnlen(timeNames[i], 7) - 1 :
-					(int)strnlen(timeNames[i], 7),
-				timeNames[i]);
-			didPrint = true;
-		}
+	if ((u = calloc(1, uptimeDataStringLength)) == nullptr)
+		err(1, "calloc");
+
+	snprintf(u, uptimeDataStringLength,
+			"%s%llu %s, %llu %s, %llu %s, %llu %s", uptimeTag,
+			timeVals[0], timeNames[0],
+			timeVals[1], timeNames[1],
+			timeVals[2], timeNames[2],
+			timeVals[3], timeNames[3]);
+	*uptimeDataString = u;
 }
 
 void
-printMemoryData(void)
+getMemoryData(char **memoryDataString)
 {
-	struct uvmexp	vm = { 0 };
-	int		mib[2] = { CTL_HW, HW_PAGESIZE };	
-	long		pageSize = 0,
-			physMem = 0;
-	long		vmSize = sizeof(struct uvmexp),
-			pageSizeSize = sizeof(pageSize),
-			physMemSize = sizeof(physMem);
+	int mib[2] = { 0 };
+	struct uvmexp vm = { 0 };
+	long pageSize = 0, physMem = 0;
+	long usedMemoryMiB = 0, totalMemoryMiB = 0;
+	short memoryDataLength = 0;
+	const char memoryTag[] = "Memory:\t";
+	char *m = nullptr;
 
-	if (sysctl(mib, 2, &pageSize, &pageSizeSize, nullptr, 0) == -1)
+	mib[0] = CTL_HW;
+	mib[1] = HW_PAGESIZE;
+	if (sysctl(mib, 2, &pageSize, (long[]){ 8 }, nullptr, 0) == -1)
 		err(1, "sysctl");
 
 	mib[0] = CTL_VM;
 	mib[1] = VM_UVMEXP;
-
-	if (sysctl(mib, 2, &vm, &vmSize, nullptr, 0) == -1)
+	if (sysctl(mib, 2, &vm, (long[]){sizeof(struct uvmexp)}, nullptr, 0) ==
+	    -1)
 		err(1, "sysctl");
 
 	mib[0] = CTL_HW;
 	mib[1] = HW_PHYSMEM64;
-
-	if (sysctl(mib, 2, &physMem, &physMemSize, nullptr, 0) == -1)
+	if (sysctl(mib, 2, &physMem, (long[]){ 8 }, nullptr, 0) == -1)
 		err(1, "sysctl");
 
-	printf("Memory:\t%ld MiB / %ld MiB",
-			(vm.active + vm.swpginuse) * pageSize / 1048576,
-							physMem / 1048576);
+	if (ckd_add(&usedMemoryMiB, vm.active, vm.swpginuse) ||
+	    ckd_mul(&usedMemoryMiB, usedMemoryMiB, pageSize))
+		errx(1, "ckd_add: integer overflow");
+
+	usedMemoryMiB /= 1048576;
+	totalMemoryMiB = physMem / 1048576;
+
+	memoryDataLength = strlen(memoryTag) + NUM_LENGTH(usedMemoryMiB) +
+				NUM_LENGTH(totalMemoryMiB) + 12;
+
+	if ((m = calloc(1, memoryDataLength)) == nullptr)
+		err(1, "calloc");
+
+	snprintf(m, memoryDataLength, "%s%ld MiB / %ld MiB", memoryTag,
+			usedMemoryMiB, totalMemoryMiB);
+	*memoryDataString = m;
 }
 
 int
 main(void)
 {
+	char *userData = nullptr, *systemData = nullptr, *hostData = nullptr,
+		*uptimeData = nullptr, *memoryData = nullptr;
+
+	getHostData(&hostData);
+
+	if (pledge("stdio rpath vminfo", nullptr) == -1)
+		err(1, "pledge");
+
+	getMemoryData(&memoryData);
+
+	if (pledge("stdio rpath", nullptr) == -1)
+		err(1, "pledge");
+
+	getUserData(&userData);
+
+	if (pledge("stdio", nullptr) == -1)
+		err(1, "pledge");
+
+	getSystemData(&systemData);
+	getUptimeData(&uptimeData);
+
 	/* -----BEGIN FISH----- */
 
 	 puts("      _____");
-	fputs("    \\-     -/\t\t", stdout);	printUserData();	NEWLINE;
-	fputs(" \\_/         \\\t\t", stdout);	printSystemData();	NEWLINE;
-	fputs(" |        O O |\t\t", stdout);	printHostData();	NEWLINE;
-	fputs(" |_  <   )  3 )\t\t", stdout);	printUptimeData();	NEWLINE;
-	fputs(" / \\         /\t\t", stdout);	printMemoryData();	NEWLINE;
-	fputs("    /-_____-\\\n\n", stdout);
+	printf("    \\-     -/\t\t%s\n", userData);
+	printf(" \\_/         \\\t\t%s\n", systemData);
+	printf(" |        O O |\t\t%s\n", hostData);
+	printf(" |_  <   )  3 )\t\t%s\n", uptimeData);
+	printf(" / \\         /\t\t%s\n", memoryData);
+	printf("    /-_____-\\\n\n");
 
 	/* -----END FISH----- */
-	
+
+	/* There's no point in freeing anything. */
+
 	return 0;
 }
